dc58d3e73477bbf267e0df228c17b5cb5043b797
kyle kersey <kylelk@comcast.net>
2016-03-27 13:56:59 -0700

    added check of GPIO setup result

--- src/main.adb
+++ src/main.adb
@@ -1,7 +1,17 @@
 with Ada.Text_IO;
+with WiringPi_H;
+with Interfaces.C;
 
 procedure main is
-    package TIO renames Ada.Text_IO;
+   use Interfaces.C;
+   package TIO renames Ada.Text_IO;
+   package GPIO renames WiringPi_H;
+   Setup_Status : Interfaces.C.Int;
 begin
-    TIO.Put_Line("hello world");
+   -- TIO.Put_Line("hello world");
+   Setup_Status := GPIO.WiringPiSetup;
+   if Setup_Status = -1 then
+      TIO.Put_Line(TIO.Standard_Error, "problem seting up GPIO");
+      return;
+   end if;
 end main;
