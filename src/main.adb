eeae2e49bd92b5cb07963320f79a10677f79536a
kyle kersey <kylelk@comcast.net>
2016-03-27 14:53:06 -0700

    added blink loop

--- src/main.adb
+++ src/main.adb
@@ -7,6 +7,7 @@ procedure main is
    package TIO renames Ada.Text_IO;
    package GPIO renames WiringPi_H;
    Setup_Status : Interfaces.C.Int;
+   Pin_Num : constant Int := Int(0);
 begin
    -- TIO.Put_Line("hello world");
    Setup_Status := GPIO.WiringPiSetup;
@@ -14,4 +15,11 @@ begin
       TIO.Put_Line(TIO.Standard_Error, "problem seting up GPIO");
       return;
    end if;
+   GPIO.PinMode(Pin_Num, Int(1));
+   loop
+      GPIO.DigitalWrite(Pin_Num, Int(1));
+      delay Duration(0.25);
+      GPIO.DigitalWrite(Pin_Num, Int(0));
+      delay Duration(0.25);
+   end loop;
 end main;
