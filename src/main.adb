0be557ca982ed6f56b18d9b59bbf754cb43674dc
kyle kersey <kylelk@comcast.net>
2016-03-27 18:15:56 -0700

    moved delay duration into a variable

--- src/main.adb
+++ src/main.adb
@@ -2,14 +2,15 @@ with GPIO;
 
 procedure main is
    Pin_Num : constant Integer := 0;
+   Blink_Delay : constant Float := 0.25;
 begin
    GPIO.Setup;
 
    GPIO.Pin_Mode(Pin_Num, GPIO.Output);
    loop
       GPIO.Digital_Write(Pin_Num, GPIO.High);
-      delay Duration(0.25);
+      delay Duration(Blink_Delay);
       GPIO.Digital_Write(Pin_Num, GPIO.Low);
-      delay Duration(0.25);
+      delay Duration(Blink_Delay);
    end loop;
 end main;
