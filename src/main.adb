ce73054de507b5d79cfa79661229dc1294009d32
kyle kersey <kylelk@comcast.net>
2016-03-31 14:32:49 -0700

    added proper code indentation

--- src/main.adb
+++ src/main.adb
@@ -19,60 +19,52 @@ procedure main is
       Current_Mode : LED_Modes := LED_Off;
    begin
       loop
-          select
-             accept Mode (Mode_Value : LED_Modes) do
-                Current_Mode := Mode_Value;
-             end Mode;
-         else 
-             if Current_Mode = LED_Off then
-                GPIO.Digital_Write (Pin_Num, GPIO.Low);
-                
-             elsif Current_Mode = LED_Blink then
-                GPIO.Digital_Write (Pin_Num, GPIO.High);
-                delay Duration (Blink_Delay);
-                GPIO.Digital_Write (Pin_Num, GPIO.Low);
-                delay Duration (Blink_Delay);
-                
-             elsif Current_Mode = LED_On then
-                GPIO.Digital_Write (Pin_Num, GPIO.High);
-             end if;
+         select
+            accept Mode (Mode_Value : LED_Modes) do
+               Current_Mode := Mode_Value;
+            end Mode;
+         else
+            if Current_Mode = LED_Off then
+               GPIO.Digital_Write (Pin_Num, GPIO.Low);
 
-        end select;
+            elsif Current_Mode = LED_Blink then
+               GPIO.Digital_Write (Pin_Num, GPIO.High);
+               delay Duration (Blink_Delay);
+               GPIO.Digital_Write (Pin_Num, GPIO.Low);
+               delay Duration (Blink_Delay);
+
+            elsif Current_Mode = LED_On then
+               GPIO.Digital_Write (Pin_Num, GPIO.High);
+            end if;
+
+         end select;
       end loop;
    end Blinking_Light;
-  
-   Light : Blinking_Light;
+
+   Light      : Blinking_Light;
    Light_Mode : LED_Modes := LED_Off;
-   Down_Press : Boolean := True;
+   Down_Press : Boolean   := True;
 
    procedure Button_Clicked is
    begin
-       if down_press then
-          Click_Count := Click_Count + 1;
-          TIO.Put_Line ("button clicked " & LED_Modes'Image(Light_Mode));
-          Light.Mode(Light_Mode);
-          if Light_Mode = LED_Modes'Last then
-              Light_Mode := LED_Modes'First;
-          else
-              Light_Mode := LED_Modes'Val(LED_Modes'Pos(Light_Mode)+1);
-          end if;
-        end if;
-        Down_Press := not down_press;
+      if Down_Press then
+         Click_Count := Click_Count + 1;
+         TIO.Put_Line ("button clicked " & LED_Modes'Image (Light_Mode));
+         Light.Mode (Light_Mode);
+         if Light_Mode = LED_Modes'Last then
+            Light_Mode := LED_Modes'First;
+         else
+            Light_Mode := LED_Modes'Val (LED_Modes'Pos (Light_Mode) + 1);
+         end if;
+      end if;
+      Down_Press := not Down_Press;
    end Button_Clicked;
-   
+
 begin
    GPIO.Setup;
    GPIO.Pin_Mode (Pin_Num, GPIO.Output);
    GPIO.Pin_Mode (Button_Pin, GPIO.Input);
 
+   -- register button callback
    GPIO.Pin_Interrupt (Button_Pin, GPIO.Edge_Both, Button_Clicked'Access);
-   
-   --Light.Mode(LED_On);
-   
-   -- loop
-   --   GPIO.Digital_Write(Pin_Num, GPIO.High);
-   --   delay Duration(Blink_Delay);
-   --   GPIO.Digital_Write(Pin_Num, GPIO.Low);
-   --   delay Duration(Blink_Delay);
-   -- end loop;
 end main;
